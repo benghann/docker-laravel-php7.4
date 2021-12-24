8e6c2cd854b5edcd90fe61804bc8036d7decefdf
Beng Han Ng <benghan.ng@Bengs-MacBook-Pro.local>
2021-12-25 00:49:21 +0800

    Initial commit

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Setting up environment files (.env) ..."  && \
+rm -rf .env && cp -rp .env.example .env && \
+echo "docker-compose up -d ..."  && \
+docker-compose up -d && \
+echo "composer install ..."  && \
+docker-compose exec --user www-data app bash -c "composer install" && \
+echo "php artisan key:generate ..."  && \
+docker-compose exec --user www-data app bash -c "php artisan key:generate" && \
+echo "php artisan migrate:fresh --seed ..."  && \
+docker-compose exec --user www-data app bash -c "php artisan migrate --seed"  && \
+echo "initialize complete..."
---
+++ shell.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker-compose exec --user www-data app bash -l
